78603de4f4747fa5bd5b3c025909a3e7346053ab
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-10-28 18:16:39 -0300

    feat(persistence): adicionar UnitOfWork com métodos de commit, rollback e dispose

---
+++ IquiriumBE.Persistence/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using IquiriumBe.Domain.Interfaces;
+
+namespace IquiriumBe.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _context;
+        private bool _disposed = false;
+
+        public UnitOfWork(DbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<int> CommitAsync()
+        {
+            try
+            {
+                return await _context.SaveChangesAsync();
+            }
+            catch (Exception ex) { 
+                throw new Exception("Ocorreu um erro ao salvar as mudanças.", ex);
+            }
+        }
+
+        public void Rollback()
+        {
+            foreach (var entry in _context.ChangeTracker.Entries())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Modified:
+                        entry.State = EntityState.Unchanged;
+                        break;
+                    case EntityState.Added:
+                        entry.State = EntityState.Detached;
+                        break;
+                    case EntityState.Deleted:
+                        entry.State = EntityState.Unchanged;
+                        break;
+                }
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
