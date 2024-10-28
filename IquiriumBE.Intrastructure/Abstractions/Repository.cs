daf3be17f478d100ca8759d7fa0ca14f271d7aaa
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-10-28 18:13:18 -0300

    feat(infra): adicionar classe abstrata Repository com operações CRUD genéricas

---
+++ IquiriumBE.Intrastructure/Abstractions/Repository.cs
@@ -0,0 +1,44 @@
+﻿using IquiriumBe.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IquiriumBe.Infrastructure.Abstractions
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly DbContext _context;
+
+        protected Repository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> GetByIdAsync(string id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            return Task.CompletedTask;
+        }
+    }
+}
