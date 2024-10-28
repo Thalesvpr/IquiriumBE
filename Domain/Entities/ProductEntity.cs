7e33385795c0bdf6f7d348544745ff2093be13c5
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-10-28 18:12:55 -0300

    feat(entity): adicionar ProductEntity com mapeamento para a tabela products

---
+++ Domain/Entities/ProductEntity.cs
@@ -0,0 +1,23 @@
+﻿using IquiriumBe.Domain.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+[Table("products")]
+public class ProductEntity : BaseEntity
+{
+    [Column("title")]
+    [Required]
+    [MaxLength(255)]
+    public required string Title { get; set; }
+
+    [Column("description", TypeName = "text")]
+    public required string Description { get; set; }
+
+    [Column("price", TypeName = "decimal(18, 2)")]
+    [Required]
+    public decimal Price { get; set; }
+
+    [Column("status")]
+    [MaxLength(50)]
+    public string Status { get; set; } = "Available";
+}
